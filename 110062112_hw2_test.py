485a93895feab703c11f42631d4d93a7d5714bf4
torrid <justinwu6900942@gmail.com>
2024-04-09 23:21:05 +0800

    submission

--- 110062112_hw2_test.py
+++ 110062112_hw2_test.py
@@ -115,7 +115,7 @@ class Agent(object):
             H=84,
             W=84
         )
-        path = "./model_best_5089.0.pth"
+        path = ".110062112_hw2_data"
         self.model.load_state_dict(torch.load(path))
         self.model.eval()
 
